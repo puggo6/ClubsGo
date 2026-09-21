2712cf598700a509e4f97a4a1cab12918e14971c
puggo6 <dogmansam2@icloud.com>
2026-09-21 15:23:01 -0400

    fixing more env

--- providers/ClerkAndConvexProvider.tsx
+++ providers/ClerkAndConvexProvider.tsx
@@ -4,12 +4,16 @@ import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import React from "react";
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+const convexUrl = process.env.EXPO_PUBLIC_CONVEX_URL;
+
 if (!publishableKey) {
   throw new Error("Missing Clerk publishable key in environment variables");
 }
-const convex = new ConvexReactClient(
-  "https://gregarious-hawk-931.convex.cloud",
-);
+if (!convexUrl) {
+  throw new Error("Missing Convex URL in environment variables");
+}
+
+const convex = new ConvexReactClient(convexUrl);
 
 export default function ClerkAndConvexProvider({
   children,
